93a49538b736b0b6002b89e406dec5e467029d38
1734387 <1734387@cstj.qc.ca>
2025-04-28 17:07:08 -0400

    timezone luxon

--- frontend/src/utils/formatDate.js
+++ frontend/src/utils/formatDate.js
@@ -1,20 +1,9 @@
+import { DateTime } from "luxon";
+
 export const formatDate = (date) => {
-  if (!date || isNaN(Date.parse(date))) return "";
+  if (!date) return "";
 
-  const options = {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: false,
-    timeZone: "America/Toronto",
-  };
-
-  const formatted = new Intl.DateTimeFormat("fr-CA", options).format(new Date(date));
-
-  const [day, month, year] = formatted.split(',')[0].split('/');
-  const time = formatted.split(',')[1].trim();
-
-  return `${year}-${month}-${day} ${time}`;
+  return DateTime.fromISO(date, { zone: "utc" })
+    .setZone("America/Toronto")
+    .toFormat("yyyy-MM-dd HH:mm");
 };
