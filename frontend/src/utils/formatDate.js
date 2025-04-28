ae36b81b74d639b6a222744b2e66b3ff00f70288
1734387 <1734387@cstj.qc.ca>
2025-04-28 13:09:41 -0400

    fix padding date

--- frontend/src/utils/formatDate.js
+++ frontend/src/utils/formatDate.js
@@ -2,15 +2,11 @@ export const formatDate = (date) => {
   if (!date || isNaN(Date.parse(date))) return "";
 
   date = new Date(date);
-  let hours = date.getHours();
-  const minutes = date.getMinutes();
-  //const ampm = hours >= 12 ? "PM" : "AM";
-  //hours = hours % 12;
-  //hours = hours ? hours : 12;
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
+  let hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
   const year = date.getFullYear();
 
-  //return `${month}/${day}/${year} ${hours}:${minutes < 10 ? "0" + minutes : minutes} ${ampm}`;
-  return `${year}-${month}-${day} ${hours}:${minutes < 10 ? "0" + minutes : minutes}`;
+  return `${year}-${month}-${day} ${hours}:${minutes}`;
 };
